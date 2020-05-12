91a359518f996a23eb9231f18019c1e8391fe479
edwardchandra <edwardchandra14417@gmail.com>
2020-05-12 13:25:51 +0700

    replace fragment with supporting files activity

---
+++ app/src/main/java/com/example/learnbit/launch/teacher/home/coursedetail/detailcontent/coursestab/supportingfiles/SupportingFilesActivity.java
@@ -0,0 +1,16 @@
+package com.example.learnbit.launch.teacher.home.coursedetail.detailcontent.coursestab.supportingfiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.learnbit.R;
+
+public class SupportingFilesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supporting_files);
+    }
+}
